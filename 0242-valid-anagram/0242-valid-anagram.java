adc117063600759d8d4d882a77912240c384758a
Surajit Rana <surajit1602@gmail.com>
2024-09-16 23:58:41 +0530

    Time: 2 ms (97.59%), Space: 44.4 MB (54.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,4 +1,14 @@
 class Solution {
+    /**
+     * Brute-Force Approach
+     * 
+     * TC: O(2N) + O(2Nlog(2N)) ~ O(Nlog(N))
+     * SC: O(2N) ~ O(N)
+     * 
+     * @param s
+     * @param t
+     * @return
+     */
     public boolean isAnagram(String s, String t) {
         char[] sch = s.toCharArray();
         char[] tch = t.toCharArray();
