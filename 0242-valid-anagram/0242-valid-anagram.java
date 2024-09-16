608569a259f9e3f4605e013f243cd3ed3eb093de
Surajit Rana <surajit1602@gmail.com>
2024-09-17 00:11:32 +0530

    Time: 16 ms (23.5%), Space: 44.4 MB (47.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,37 @@
 class Solution {
     /**
+     * Optimal Approach
+     * 
+     * TC: O(2N) ~ O(N)
+     * SC: O(N)
+     * 
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> hm = new HashMap<Character, Integer>(); // SC: O(N)
+        for (int i = 0; i < s.length(); i++) { // TC: O(N)
+            char ch = s.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) { // TC: O(N)
+            char ch = t.charAt(i);
+            if (!hm.containsKey(ch)) {
+                return false;
+            } else {
+                int freq = hm.get(ch);
+                if (freq > 1) {
+                    hm.put(ch, freq - 1);
+                } else {
+                    hm.remove(ch);
+                }
+            }
+        }
+        return hm.isEmpty();
+    }
+
+    /**
      * Brute-Force Approach
      * 
      * TC: O(2N) + O(2Nlog(2N)) ~ O(Nlog(N))
@@ -9,7 +41,7 @@ class Solution {
      * @param t
      * @return
      */
-    public boolean isAnagram(String s, String t) {
+    public boolean isAnagramBruteForce(String s, String t) {
         char[] sch = s.toCharArray();
         char[] tch = t.toCharArray();
         Arrays.sort(sch);
