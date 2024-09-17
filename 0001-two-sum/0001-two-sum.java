10f82daf52f43f15686034dc163273966d874fd9
Surajit Rana <surajit1602@gmail.com>
2024-09-17 20:58:34 +0530

    Time: 2 ms (98.55%), Space: 45.1 MB (19.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,39 @@
 class Solution {
+    /**
+     * Optimal Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int diff = target - nums[i];
+            if (hm.containsKey(diff)) {
+                return new int[] { hm.get(diff), i };
+            } else {
+                hm.put(nums[i], i);
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    /**
+     * Brute-Force Approach
+     * 
+     * TC: O(N^2)
+     * SC: O(1)
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSumBruteForce(int[] nums, int target) {
         int n = nums.length;
         for (int i = 0; i < n; i++) { // TC: O(N)
             for (int j = i + 1; j < n; j++) { // TC: O(N)
